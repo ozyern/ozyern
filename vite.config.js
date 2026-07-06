d814fa4552964a202b1cb5052fd27013dedf2cc9
Sabrina AnnLynn Carpenter <sabrina.ozyern@github.com>
2026-07-06 17:35:27 +0530

    fix: adjust vite config and index for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  }
 })
